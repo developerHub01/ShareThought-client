4441875fbd1d11169985bc2fbf0f94eb1a978b8e
developerHub01 <developerhubytchannel@gmail.com>
2024-11-03 01:09:30 +0600

    PostCard design bug fixed

--- src/components/cards/PostCard.tsx
+++ src/components/cards/PostCard.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-import PostCardCTA from "../actions/PostCardCTA";
+import PostCardCTA from "@/components/actions/PostCardCTA";
 
 const PostCard = () => {
   const imgUrl =
     "https://images.unsplash.com/photo-1730217804424-825f12eef36f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   return (
-    <div className="shadow-xl w-full max-w-sm border border-gray-500/10 rounded-sm">
+    <div className="shadow-xl w-full max-w-sm border border-gray-500/10 rounded-sm overflow-hidden">
       <Link href={"/"} className="flex justify-start items-center gap-2 p-2.5">
         <div className="size-10 overflow-hidden aspect-square rounded-full flex-shrink-0">
           <Image
@@ -22,7 +22,7 @@ const PostCard = () => {
         </h4>
       </Link>
       <Link href={"/"}>
-        <div className="aspect-video">
+        <div className="aspect-video rounded-t-sm overflow-hidden">
           <Image
             src={imgUrl}
             width={300}
