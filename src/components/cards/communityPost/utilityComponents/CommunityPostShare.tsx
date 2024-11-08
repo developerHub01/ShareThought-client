142b9e81af7101b32aa8b997402cea2142bd17c3
developerHub01 <developerhubytchannel@gmail.com>
2024-11-08 22:36:43 +0600

    CommunityPostShare component created

---
+++ src/components/cards/communityPost/utilityComponents/CommunityPostShare.tsx
@@ -0,0 +1,11 @@
+import HorizontalPostCard from "../../post/HorizontalPostCard";
+
+const CommunityPostShare = () => {
+  return (
+    <div className="w-full">
+      <HorizontalPostCard />
+    </div>
+  );
+};
+
+export default CommunityPostShare;
