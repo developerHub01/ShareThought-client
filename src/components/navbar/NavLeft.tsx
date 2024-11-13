f6b461af4d3440b8e4c9b52463b505f5052b8c82
developerHub01 <developerhubytchannel@gmail.com>
2024-11-14 00:42:30 +0600

    nav notification portion created

---
+++ src/components/navbar/NavLeft.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const NavLeft = () => {
+  return (
+    <div>
+      <Link href="/">ShareThought</Link>
+    </div>
+  );
+};
+
+export default NavLeft;
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import NavLeft from "@/components/navbar/NavLeft";
+import Searchbar from "@/components/navbar/Searchbar";
+import NavRight from "@/components/navbar/NavRight";
+
+const Navbar = () => {
+  return (
+    <header className="w-full p-2 sticky top-0 z-50">
+      <div className="flex justify-between items-center gap-2 w-full mx-auto px-5 shadow-lg rounded-sm p-2 border bg-white">
+        <NavLeft />
+        <Searchbar />
+        <NavRight />
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
