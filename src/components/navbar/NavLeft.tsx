e167a3b462125bf141c06022429b6bb0defeb08a
developerHub01 <developerhubytchannel@gmail.com>
2024-11-14 04:44:17 +0600

    AppSidebarTrigger added in nav left portion

--- src/components/navbar/NavLeft.tsx
+++ src/components/navbar/NavLeft.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
+import AppSidebarTrigger from "@/components/sidebar/AppSidebarTrigger";
 
 const NavLeft = () => {
   return (
-    <div>
-      <Link href="/">ShareThought</Link>
+    <div className="flex justify-start items-center gap-1">
+      <div className="block sm:hidden">
+        <AppSidebarTrigger className="rounded-full" />
+      </div>
+      <Link href="/" className="select-none font-bold">
+        ShareThought
+      </Link>
     </div>
   );
 };
