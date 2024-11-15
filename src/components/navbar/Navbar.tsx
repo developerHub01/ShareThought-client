bc05703e740f691a3d9295db347b1ca5a779b44a
developerHub01 <developerhubytchannel@gmail.com>
2024-11-15 09:07:56 +0600

    navbar spacing managed for small screen

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,8 +4,8 @@ import NavRight from "@/components/navbar/NavRight";
 
 const Navbar = () => {
   return (
-    <header className="w-full p-2 sticky top-0 z-50">
-      <div className="flex justify-between items-center gap-2 w-full mx-auto px-1 sm:px-5 shadow-lg rounded-sm p-2 border bg-white relative overflow-hidden">
+    <header className="w-full p-1.5 sm:p-2 sticky top-0 z-50">
+      <div className="flex justify-between items-center gap-1 sm:gap-2 w-full mx-auto px-1 sm:px-5 shadow-lg rounded-sm p-1.5 sm:p-2 border bg-white relative overflow-hidden">
         <NavLeft />
         <Searchbar />
         <NavRight />
