1c3799d5beab4dc78cbd31468137244211ff88c8
developerHub01 <developerhubytchannel@gmail.com>
2024-11-17 01:25:56 +0600

    notification button counter added

--- src/components/navbar/right/notification/NotificationButton.tsx
+++ src/components/navbar/right/notification/NotificationButton.tsx
@@ -1,16 +1,26 @@
 import { Button } from "@/components/ui/button";
 import { Bell as NotificationIcon } from "lucide-react";
 import NotificationPopover from "@/components/navbar/right/notification/NotificationPopover";
+import { Badge } from "@/components/ui/badge";
 
 const NotificationButton = () => {
+  const newNotificationCount = 100;
   return (
     <NotificationPopover>
       <Button
         size={"icon"}
         variant={"ghost"}
-        className="rounded-full flex-shrink-0"
+        className="rounded-full flex-shrink-0 relative"
       >
-        <NotificationIcon size={20} strokeWidth={1.5} />
+        {newNotificationCount > 0 && (
+          <Badge
+            className="absolute p-[2.5px] text-[10px] font-light top-0 right-0 translate-x-1/2 flex-shrink-0 leading-none w-7 flex justify-center items-center line-clamp-1 overflow-hidden"
+            size={"sm"}
+          >
+            {newNotificationCount >= 10 ? "10+" : newNotificationCount}
+          </Badge>
+        )}
+        <NotificationIcon size={22} strokeWidth={1.5} />
       </Button>
     </NotificationPopover>
   );
