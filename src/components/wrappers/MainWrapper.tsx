0db2f428aa77d3fabe564f174a14f222637079e9
developerHub01 <developerhubytchannel@gmail.com>
2024-11-17 13:55:42 +0600

    MainWrapper component created

---
+++ src/components/wrappers/MainWrapper.tsx
@@ -0,0 +1,16 @@
+import clsx from "clsx";
+import React from "react";
+
+interface MainWrapperProps {
+  children: React.ReactNode;
+  className?: string;
+}
+const MainWrapper = ({ children, className = "" }: MainWrapperProps) => {
+  return (
+    <main className={clsx("w-full h-full p-2 md:p-0 md:pr-2", className)}>
+      {children}
+    </main>
+  );
+};
+
+export default MainWrapper;
