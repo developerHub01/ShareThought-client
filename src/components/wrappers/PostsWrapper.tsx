58fc79b5f5e004bbe6d0757b9615ecb6fa9159cb
developerHub01 <developerhubytchannel@gmail.com>
2024-11-17 14:15:53 +0600

    PostsWrapper component created

---
+++ src/components/wrappers/PostsWrapper.tsx
@@ -0,0 +1,22 @@
+import clsx from "clsx";
+import React from "react";
+
+interface PostsWrapperProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const PostsWrapper = ({ children, className = "" }: PostsWrapperProps) => {
+  return (
+    <section
+      className={clsx(
+        "grid gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4",
+        className
+      )}
+    >
+      {children}
+    </section>
+  );
+};
+
+export default PostsWrapper;
