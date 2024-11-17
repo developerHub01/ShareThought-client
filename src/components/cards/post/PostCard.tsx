07371a314c00e10cfed5b635218b7c4dcdab818c
developerHub01 <developerhubytchannel@gmail.com>
2024-11-17 14:15:31 +0600

    PostCard style updated

--- src/components/cards/post/PostCard.tsx
+++ src/components/cards/post/PostCard.tsx
@@ -7,9 +7,9 @@ const PostCard = ({ showThreeDot = true }: { showThreeDot?: boolean }) => {
   const imgUrl =
     "https://images.unsplash.com/photo-1730217804424-825f12eef36f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   return (
-    <div className="shadow-xl w-full max-w-sm border border-accent rounded-sm overflow-hidden">
+    <div className="shadow-xl w-full border border-accent rounded-sm overflow-hidden">
       <Link href={"/"} className="flex justify-start items-center gap-2 p-2.5">
-        <div className="size-10 overflow-hidden aspect-square rounded-full flex-shrink-0">
+        <div className="size-8 md:size-10 overflow-hidden aspect-square rounded-full flex-shrink-0">
           <Image
             src={imgUrl}
             width={80}
@@ -18,7 +18,9 @@ const PostCard = ({ showThreeDot = true }: { showThreeDot?: boolean }) => {
             className="size-full object-cover select-none"
           />
         </div>
-        <h4 className="text-primary/80 text-base font-semibold line-clamp-1 overflow-hidden text-ellipsis">
+        <h4
+          className="text-primary/80 text-sm font-semibold line-clamp-1 overflow-hidden text-ellipsis"
+        >
           Developer Hub BD
         </h4>
       </Link>
@@ -28,7 +30,7 @@ const PostCard = ({ showThreeDot = true }: { showThreeDot?: boolean }) => {
           width={300}
           height={300}
           alt=""
-          className="size-full object-cover select-none"
+          className="w-full object-cover select-none"
         />
       </Link>
       <div className="p-2.5 py-3 flex justify-between gap-2">
