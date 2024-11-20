f9a515e8ccf238e9fbdb469744f4c22ecc77bf16
developerHub01 <developerhubytchannel@gmail.com>
2024-11-21 03:55:59 +0600

    ModelWrapper added in singup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SignUpLeft from "@/app/signup/_components/SignUpLeft";
 import SignUpRight from "@/app/signup/_components/SignUpRight";
+import ModelWrapper from "@/app/signup/_components/modal/ModelWrapper";
 
 const RegisterPage = () => {
   return (
     <section className="w-full min-h-screen h-full flex flex-col md:flex-row">
       <SignUpLeft />
       <SignUpRight />
+      <ModelWrapper />
     </section>
   );
 };
