f09b581ba2fffa8f66d8f14d663674ff060c2d52
developerHub01 <developerhubytchannel@gmail.com>
2024-11-27 19:41:13 +0600

    Input shadcn component style updated

--- src/components/Inputs/Input.tsx
+++ src/components/Inputs/Input.tsx
@@ -8,7 +8,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "flex w-full tracking-wide rounded-sm bg-accent px-3 py-2.5 text-base shadow-inner file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm placeholder:select-none",
+          "flex w-full tracking-wide rounded-sm bg-accent px-3 py-2.5 shadow-inner file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 text-sm placeholder:select-none",
           className
         )}
         ref={ref}
