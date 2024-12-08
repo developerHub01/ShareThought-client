5addea361b3164dd23c229c4a4ecd92f9db595b5
developerHub01 <developerhubytchannel@gmail.com>
2024-12-08 14:21:52 +0600

    AvatarUploaderModal drag and remove drag canvas effect added

--- src/app/signup/_components/modal/AvatarUploaderModal.tsx
+++ src/app/signup/_components/modal/AvatarUploaderModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { DragEvent, ChangeEvent, Fragment } from "react";
+import React, { DragEvent, ChangeEvent, Fragment, useState } from "react";
 import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
@@ -14,10 +14,10 @@ import {
   Trash as RemoveIcon,
   CheckCheck as ProceedIcon,
 } from "lucide-react";
-import {
-  TooltipProvider,
-} from "@/components/ui/tooltip";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import ActionButton from "@/app/signup/_components/modal/ActionButton";
+import { CenterScrollArea } from "@/components/scrollArea/CenterScrollArea";
+import clsx from "clsx";
 
 interface IActionButton {
   id: "camera" | "edit" | "remove" | "proceed";
@@ -56,11 +56,15 @@ const actionButtonList: Array<IActionButton> = [
 ];
 
 const AvatarUploaderModal = () => {
+  const [isDragging, setIsDragging] = useState(false);
   const { avatar: avatarPreview } = useAppSelector((state) => state.signUp);
   const dispatch = useAppDispatch();
   const { toast } = useToast();
 
-  const handleClearAvatar = () => dispatch(removeAvatar());
+  const handleClearAvatar = () => {
+    dispatch(removeAvatar());
+    setIsDragging(false);
+  };
 
   actionButtonList[2].onClick = handleClearAvatar;
 
@@ -72,7 +76,15 @@ const AvatarUploaderModal = () => {
     if (avatarFile) processAvatarFile(avatarFile);
   };
 
-  const handleDragOver = (e: DragEvent<HTMLDivElement>) => e.preventDefault();
+  const handleDragOver = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = (e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setIsDragging(false);
+  };
 
   const handleAvatarChange = (e: ChangeEvent<HTMLInputElement>) => {
     const avatarFile = e.target.files?.[0];
@@ -93,79 +105,91 @@ const AvatarUploaderModal = () => {
   };
 
   return (
-    <section className="w-full h-full flex flex-col justify-center items-center gap-3 max-w-lg">
-      <div>
-        <h2 className="text-center capitalize text-primary text-xl font-bold select-none">
-          Upload avatar
-        </h2>
-      </div>
-      <LoginSeparator />
-      <label
-        htmlFor="avatar"
-        className="w-full max-w-sm max-h-sm p-2 group rounded-md"
-      >
-        <div
-          onDrop={handleDrop}
-          onDragOver={handleDragOver}
-          className="size-full aspect-square flex flex-col gap-5 justify-center items-center bg-accent rounded-md cursor-pointer ring-2 border-dashed duration-100 ring-offset-0 group-hover:ring-offset-4 ring-transparent group-hover:ring-primary/50 shadow-lg p-2 text-center"
+    <CenterScrollArea className="h-full p-5">
+      <section className="w-full h-full flex flex-col justify-center items-center gap-3 max-w-lg">
+        <div>
+          <h2 className="text-center capitalize text-primary text-xl font-bold select-none">
+            Upload avatar
+          </h2>
+        </div>
+        <LoginSeparator />
+        <label
+          htmlFor="avatar"
+          className="w-full max-w-[350px] max-h-[350px] p-2 group rounded-md"
         >
-          {avatarPreview ? (
-            <Image
-              src={avatarPreview}
-              width={400}
-              height={400}
-              alt=""
-              className="size-full object-cover rounded-sm"
-            />
-          ) : (
-            <UploadAvatarCanvas />
-          )}
-        </div>
-      </label>
-      <LoginSeparator />
-      <TooltipProvider>
-        <div className="flex justify-center items-center gap-2 flex-wrap">
-          {actionButtonList.map(
-            ({ id, label, Icon, link, onClick, isInput }) => {
-              const actionButtonProps = { id, Icon, onClick, label };
+          <div
+            onDrop={handleDrop}
+            onDragOver={handleDragOver}
+            onDragLeave={handleDragLeave}
+            className={clsx(
+              "size-full aspect-square flex flex-col gap-5 justify-center items-center rounded-md cursor-pointer border-dashed duration-100 ring-offset-0 group-hover:ring-offset-4 ring-transparent group-hover:ring-primary/50 shadow-lg p-2 text-center",
+              {
+                "ring-2 ring-offset-4 ring-primary/50 bg-accent": isDragging,
+                "ring-0 bg-transparent": !isDragging,
+              }
+            )}
+          >
+            {avatarPreview ? (
+              <Image
+                src={avatarPreview}
+                width={400}
+                height={400}
+                alt=""
+                className="size-full object-cover rounded-sm"
+              />
+            ) : (
+              <UploadAvatarCanvas />
+            )}
+          </div>
+        </label>
+        <LoginSeparator />
+        <TooltipProvider>
+          <div className="flex justify-center items-center gap-2 flex-wrap">
+            {actionButtonList.map(
+              ({ id, label, Icon, link, onClick, isInput }) => {
+                if (
+                  !avatarPreview &&
+                  ["edit", "remove", "proceed"].includes(id)
+                )
+                  return null;
 
-              if (!avatarPreview && ["edit", "remove", "proceed"].includes(id))
-                return null;
+                const actionButtonProps = { id, Icon, onClick, label };
 
-              return (
-                <Fragment key={id}>
-                  {isInput ? (
-                    <label htmlFor="avatar" className="cursor-pointer">
-                      <input
-                        type="file"
-                        name="avatar"
-                        accept="image/*"
-                        id="avatar"
-                        onChange={handleAvatarChange}
-                        hidden
-                      />
-                      <ActionButton
-                        {...actionButtonProps}
-                        className="pointer-events-none"
-                      />
-                    </label>
-                  ) : (
-                    <>
-                      {link && (
-                        <Link href={"/signup?avatar=edit"}>
-                          <ActionButton {...actionButtonProps} />
-                        </Link>
-                      )}
-                      {onClick && <ActionButton {...actionButtonProps} />}
-                    </>
-                  )}
-                </Fragment>
-              );
-            }
-          )}
-        </div>
-      </TooltipProvider>
-    </section>
+                return (
+                  <Fragment key={id}>
+                    {isInput ? (
+                      <label htmlFor="avatar" className="cursor-pointer">
+                        <input
+                          type="file"
+                          name="avatar"
+                          accept="image/*"
+                          id="avatar"
+                          onChange={handleAvatarChange}
+                          hidden
+                        />
+                        <ActionButton
+                          {...actionButtonProps}
+                          className="pointer-events-none"
+                        />
+                      </label>
+                    ) : (
+                      <>
+                        {link && (
+                          <Link href={"/signup?avatar=edit"}>
+                            <ActionButton {...actionButtonProps} />
+                          </Link>
+                        )}
+                        {onClick && <ActionButton {...actionButtonProps} />}
+                      </>
+                    )}
+                  </Fragment>
+                );
+              }
+            )}
+          </div>
+        </TooltipProvider>
+      </section>
+    </CenterScrollArea>
   );
 };
 
