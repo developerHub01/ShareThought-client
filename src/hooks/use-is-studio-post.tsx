1122eb79ff71a46b3dea36ed2efb4be3e45b7de4
developerHub01 <developerhubytchannel@gmail.com>
2024-12-13 20:52:57 +0600

    hook bug solved

--- src/hooks/use-is-studio-post.tsx
+++ src/hooks/use-is-studio-post.tsx
@@ -2,7 +2,8 @@ import { usePathname } from "next/navigation";
 
 const useIsStudioPost = () => {
   const pathname = usePathname();
-  return Boolean(pathname?.split("/studio/")[1]?.split("/")[0]);
+
+  return Boolean(pathname?.split("/studio/")[1]?.split("/")[0] === "post");
 };
 
 export default useIsStudioPost;
