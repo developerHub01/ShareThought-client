3c8acf7693dcf85ec485c6f8a0caa838daa60d31
developerHub01 <developerhubytchannel@gmail.com>
2024-12-15 07:37:57 +0600

    studio navbar create popover created

--- src/components/navbar/right/NavRight.tsx
+++ src/components/navbar/right/NavRight.tsx
@@ -4,9 +4,18 @@ import NotificationButton from "@/components/navbar/right/notification/Notificat
 import AvatarButton from "@/components/navbar/right/avatar/AvatarButton";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import LoginWrapper from "@/components/navbar/right/login/LoginWrapper";
 import useIsStudio from "@/hooks/use-is-studio";
-import { Plus as AddIcon } from "lucide-react";
+import {
+  Plus as AddIcon,
+  SquarePen as CreateIcon,
+  LucideIcon,
+} from "lucide-react";
 
 const NavRight = () => {
   const showLoginOption = false;
@@ -34,17 +43,48 @@ const AuthenticatedUserButtonList = () => {
 
   return (
     <>
-      {isStudio && (
-        <Link href={"/"}>
-          <Button size="sm">
-            <AddIcon />
-            <span className="hidden sm:block">Create Post</span>
-          </Button>
-        </Link>
-      )}
+      {isStudio && <CreateButtonPopover />}
       <NotificationButton />
       <AvatarButton />
     </>
+  );
+};
+
+const CreateButtonPopover = () => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="outline" size={"sm"}>
+          <AddIcon size={20} />
+          <span className="hidden sm:block">Create</span>
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="flex flex-col gap-1 w-fit p-1" align="end">
+        <CreateButton href="/" Icon={CreateIcon} label="Create Blog Post" />
+        <CreateButton
+          href="/"
+          Icon={CreateIcon}
+          label="Create Community Post"
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+interface CreateButtonProps {
+  href: string;
+  Icon: LucideIcon;
+  label: string;
+}
+
+const CreateButton = ({ href = "/", Icon, label }: CreateButtonProps) => {
+  return (
+    <Link href={href} className="w-full">
+      <Button variant={"ghost"} size={"sm"} className="w-full justify-start">
+        <Icon size={20} />
+        <span>{label}</span>
+      </Button>
+    </Link>
   );
 };
 
