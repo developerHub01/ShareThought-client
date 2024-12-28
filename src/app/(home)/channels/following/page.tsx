a2787fb8cd59d22bf008bc5c623c6140d47e90ed
developerHub01 <developerhubytchannel@gmail.com>
2024-12-28 20:37:17 +0600

    following channels and my channels page created

--- src/app/(home)/channels/following/page.tsx
+++ src/app/(home)/channels/following/page.tsx
@@ -1,7 +1,23 @@
+import FollowingChannelCard from "@/components/cards/channel/FollowingChannelCard";
+import Heading_v1 from "@/components/headings/Heading_v1";
+import PageHeadingWrapper from "@/components/headings/PageHeadingWrapper";
 import React from "react";
 
 const FollowingChannels = () => {
-  return <div>FollowingChannels</div>;
+  return (
+    <section className="mx-auto w-full max-w-7xl">
+      <PageHeadingWrapper>
+        <Heading_v1>My Following Channels</Heading_v1>
+      </PageHeadingWrapper>
+      <div className="w-full grid lg:grid-cols-2 gap-5">
+        {Array(20)
+          .fill(0)
+          .map((_, index) => (
+            <FollowingChannelCard key={index} />
+          ))}
+      </div>
+    </section>
+  );
 };
 
 export default FollowingChannels;
--- src/app/(home)/channels/my/page.tsx
+++ src/app/(home)/channels/my/page.tsx
@@ -1,7 +1,29 @@
+import MyChannelCard from "@/components/cards/channel/MyChannelCard";
+import { Button } from "@/components/ui/button";
 import React from "react";
+import { Plus as CreateIcon } from "lucide-react";
+import Heading_v1 from "@/components/headings/Heading_v1";
+import PageHeadingWrapper from "@/components/headings/PageHeadingWrapper";
 
 const MyChannels = () => {
-  return <div>MyChannels</div>;
+  return (
+    <section className="mx-auto w-full max-w-7xl">
+      <PageHeadingWrapper>
+        <Heading_v1>My Channels</Heading_v1>
+        <Button size={"sm"}>
+          <CreateIcon size={16} /> Create Channel
+        </Button>
+      </PageHeadingWrapper>
+
+      <div className="w-full grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {Array(20)
+          .fill(0)
+          .map((_, index) => (
+            <MyChannelCard key={index} />
+          ))}
+      </div>
+    </section>
+  );
 };
 
 export default MyChannels;
