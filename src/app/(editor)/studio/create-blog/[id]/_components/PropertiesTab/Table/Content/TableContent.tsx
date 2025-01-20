4194bca571a2c71875ca341f941f1fa84b011ecf
developerHub01 <developerhubytchannel@gmail.com>
2025-01-20 13:44:37 +0600

    Editor table content fontsize property added

--- src/app/(editor)/studio/create-blog/[id]/_components/PropertiesTab/Table/Content/TableContent.tsx
+++ src/app/(editor)/studio/create-blog/[id]/_components/PropertiesTab/Table/Content/TableContent.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import TableContentFontWeight from "@/app/(editor)/studio/create-blog/[id]/_components/PropertiesTab/Table/Content/TableContentFontWeight";
+import TableContentFontSize from "@/app/(editor)/studio/create-blog/[id]/_components/PropertiesTab/Table/Content/TableContentFontSize";
 
 const TableContent = () => {
   return (
     <div className="flex flex-col h-full">
       <TableContentFontWeight />
+      <TableContentFontSize />
     </div>
   );
 };
---
+++ src/app/(editor)/studio/create-blog/[id]/_components/PropertiesTab/Table/Content/TableContentFontSize.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import React, { ChangeEvent } from "react";
+import CountBlock from "@/app/(editor)/studio/create-blog/[id]/_components/Blocks/CountBlock";
+import { EDITOR_TABLE_SIZE } from "@/constant";
+import {
+  changeTableContentStyle,
+  TableInterface,
+} from "@/redux/features/builders/blogBuilderSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { useParams } from "next/navigation";
+
+const TableContentFontSize = () => {
+  const dispatch = useAppDispatch();
+  const { id: blogId } = useParams() as { id: string };
+
+  if (!blogId) return null;
+
+  const { activeBlock, components } = useAppSelector(
+    (state) => state.blogBuilder.blogs[blogId as string]
+  );
+
+  if (!activeBlock) return null;
+
+  const tableData = components[activeBlock as string]
+    ?.children as TableInterface;
+
+  const tableContent = tableData.content;
+
+  /* Columns handlers =========== */
+  const handleFontSizeIncrement = () => {
+    dispatch(
+      changeTableContentStyle({
+        blogId,
+        id: activeBlock,
+        fontSize: "inc",
+      })
+    );
+  };
+
+  const handleFontSizeDecrement = () => {
+    dispatch(
+      changeTableContentStyle({
+        blogId,
+        id: activeBlock,
+        fontSize: "dec",
+      })
+    );
+  };
+
+  const handleFontSizeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = Number(e.target.value);
+    /* if columns count is 0 or less then 1 and if greater then 8 then max value 8 */
+    const fontSize =
+      value < EDITOR_TABLE_SIZE.MIN_CONTENT_FONT_SIZE
+        ? EDITOR_TABLE_SIZE.MIN_CONTENT_FONT_SIZE
+        : value > EDITOR_TABLE_SIZE.MAX_CONTENT_FONT_SIZE
+        ? EDITOR_TABLE_SIZE.MAX_CONTENT_FONT_SIZE
+        : value;
+
+    dispatch(
+      changeTableContentStyle({
+        blogId,
+        id: activeBlock,
+        fontSize: fontSize,
+      })
+    );
+  };
+
+  return (
+    <CountBlock
+      title="Font Size"
+      value={
+        tableContent?.fontSize || EDITOR_TABLE_SIZE.DEFAULT_CONTENT_FONT_SIZE
+      }
+      handleIncrement={handleFontSizeIncrement}
+      handleDecrement={handleFontSizeDecrement}
+      handleChange={handleFontSizeChange}
+      min={EDITOR_TABLE_SIZE.MIN_CONTENT_FONT_SIZE}
+      max={EDITOR_TABLE_SIZE.MAX_CONTENT_FONT_SIZE}
+    />
+  );
+};
+
+export default TableContentFontSize;
