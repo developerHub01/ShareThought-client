5bc74209384c745903583f98ba29497a77acecc6
developerHub01 <developerhubytchannel@gmail.com>
2025-02-17 01:23:01 +0600

    update: Component list added in component sidebar as a tab.

--- src/app/(editor)/studio/create-blog/[id]/_components/ComponentButton.tsx
+++ src/app/(editor)/studio/create-blog/[id]/_components/ComponentButton.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 
 import { LucideIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface ComponentButtonProps {
+  className?: string;
   label: string;
-  onClick: () => void;
+  onClick?: () => void;
   Icon: LucideIcon | React.ComponentType<unknown>;
 }
 
-const ComponentButton = ({ label, onClick, Icon }: ComponentButtonProps) => {
+const ComponentButton = ({
+  className = "",
+  label,
+  onClick = () => {},
+  Icon,
+}: ComponentButtonProps) => {
   return (
     <button
       type="button"
-      className="flex flex-col bg-accent rounded-sm p-2 justify-center items-center gap-2 aspect-square ring-2 ring-transparent hover:ring-primary/50 duration-150 shadow-md hover:shadow-2xl"
+      className={cn(
+        "flex flex-col bg-accent rounded-sm p-2 justify-center items-center gap-2 aspect-square ring-2 ring-transparent hover:ring-primary/50 duration-150 shadow-md hover:shadow-2xl",
+        className
+      )}
       onClick={onClick}
     >
       <Icon size={35} />
--- src/app/(editor)/studio/create-blog/[id]/_components/ComponentsTab/ComponentsTab.tsx
+++ src/app/(editor)/studio/create-blog/[id]/_components/ComponentsTab/ComponentsTab.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { useEditor } from "@/app/(editor)/studio/create-blog/[id]/_components/EditorProvider";
+import { useAppDispatch } from "@/redux/hooks";
+import {
+  addComponent,
+  BlockTypes,
+} from "@/redux/features/builders/blogBuilderSlice";
+import { useParams } from "next/navigation";
+import ComponentButton from "@/app/(editor)/studio/create-blog/[id]/_components/ComponentButton";
+import { componentItemList } from "@/app/(editor)/studio/create-blog/[id]/_constant";
 
 const ComponentsTab = () => {
-  return (
-    <div>ComponentsTab</div>
-  )
-}
+  const { id: blogId } = useParams<{ id: string }>();
 
-export default ComponentsTab
+  const dispatch = useAppDispatch();
+
+  const { setIsComponentDialogOpen, selectedIndex, selectedParentId } =
+    useEditor();
+
+  const handleClick = (blockId: BlockTypes) => {
+    setIsComponentDialogOpen(false);
+    dispatch(
+      addComponent({
+        id: blogId,
+        type: blockId,
+        index: selectedIndex,
+        parentId: selectedParentId,
+      })
+    );
+  };
+
+  return (
+    <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-3 sm:gap-5 p-2">
+      <TooltipProvider>
+        {componentItemList.map(({ id, label, Icon }) => (
+          <Tooltip key={id}>
+            <TooltipTrigger asChild>
+              <ComponentButton
+                className="cursor-grabbing"
+                label={label}
+                Icon={Icon}
+                onClick={() => handleClick(id)}
+              />
+            </TooltipTrigger>
+            <TooltipContent side="bottom" sideOffset={10}>
+              <p>{label}</p>
+            </TooltipContent>
+          </Tooltip>
+        ))}
+      </TooltipProvider>
+    </div>
+  );
+};
+
+export default ComponentsTab;
