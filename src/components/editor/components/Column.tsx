e5a1cdeba6d425439acd5368efe3f41509a702ad
developerHub01 <developerhubytchannel@gmail.com>
2025-02-18 06:49:45 +0600

    update: Active Component indicator included into Column lavel
    Wrap Column component in BlockComponentWrapper for improved styling and structure

--- src/components/editor/components/Column.tsx
+++ src/components/editor/components/Column.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import BlockComponent from "@/app/(editor)/studio/create-blog/[id]/_components/BlockComponent";
+import BlockComponentWrapper from "@/app/(editor)/studio/create-blog/[id]/_components/BlockComponentWrapper";
 import AddComponentSection from "@/app/(editor)/studio/create-blog/[id]/_components/BuilderPopover/AddComponentSection";
 import { useAppSelector } from "@/redux/hooks";
 import { AnimatePresence, motion } from "motion/react";
@@ -23,29 +24,31 @@ const Column = ({ id, ...props }: RowProps) => {
   const { children } = components[id];
 
   return (
-    <section className="w-full flex flex-col border border-red-500">
-      {Array.isArray(children) && (
-        <>
-          {Boolean(children.length) && <AddComponentSection index={0} />}
-          {children.map((id, index, list) => (
-            <div key={id} className="group">
-              <BlockComponent id={id} />
-              <AnimatePresence>
-                {index !== list.length - 1 && (
-                  <motion.div
-                    className="group-hover:opacity-100 group-hover:scale-y-100 opacity-0 scale-y-0 -translate-y-1/2 mt-1 mx-auto"
-                    exit={{ opacity: 0 }}
-                  >
-                    <AddComponentSection index={index + 1} parentId={id} />
-                  </motion.div>
-                )}
-              </AnimatePresence>
-            </div>
-          ))}
-          <AddComponentSection index={children.length} parentId={id} />
-        </>
-      )}
-    </section>
+    <BlockComponentWrapper id={id} className="w-full max-w-3xl rounded-sm">
+      <section className="w-full flex flex-col border">
+        {Array.isArray(children) && (
+          <>
+            {Boolean(children.length) && <AddComponentSection index={0} />}
+            {children.map((id, index, list) => (
+              <div key={id} className="group">
+                <BlockComponent id={id} />
+                <AnimatePresence>
+                  {index !== list.length - 1 && (
+                    <motion.div
+                      className="group-hover:opacity-100 group-hover:scale-y-100 opacity-0 scale-y-0 -translate-y-1/2 mt-1 mx-auto"
+                      exit={{ opacity: 0 }}
+                    >
+                      <AddComponentSection index={index + 1} parentId={id} />
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </div>
+            ))}
+            <AddComponentSection index={children.length} parentId={id} />
+          </>
+        )}
+      </section>
+    </BlockComponentWrapper>
   );
 };
 
