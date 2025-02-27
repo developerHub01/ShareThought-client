11cac8e0d7311f3d6182722d07e0af5e653cc955
developerHub01 <developerhubytchannel@gmail.com>
2025-02-27 14:36:27 +0600

    add: Editor Implement ImageObjectFit property for customizable image fitting options

--- src/app/(editor)/studio/create-blog/[id]/_components/PropertiesTab/Image/Layout/ImageLayout.tsx
+++ src/app/(editor)/studio/create-blog/[id]/_components/PropertiesTab/Image/Layout/ImageLayout.tsx
@@ -3,12 +3,14 @@ import AlignmentProperty from "@/app/(editor)/studio/create-blog/[id]/_component
 import ImageWidth from "@/app/(editor)/studio/create-blog/[id]/_components/PropertiesTab/Image/Layout/ImageWidth";
 import ImageHeight from "@/app/(editor)/studio/create-blog/[id]/_components/PropertiesTab/Image/Layout/ImageHeight";
 import PropertyTypeWrapper from "@/app/(editor)/studio/create-blog/[id]/_components/PropertiesTab/Common/PropertyTypeWrapper";
+import ImageObjectFit from "@/app/(editor)/studio/create-blog/[id]/_components/PropertiesTab/Image/Layout/ImageObjectFit";
 
 const ImageLayout = () => {
   return (
     <PropertyTypeWrapper>
       <ImageWidth />
       <ImageHeight />
+      <ImageObjectFit />
       <AlignmentProperty />
     </PropertyTypeWrapper>
   );
---
+++ src/app/(editor)/studio/create-blog/[id]/_components/PropertiesTab/Image/Layout/ImageObjectFit.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import React, { CSSProperties } from "react";
+import SelectBlock from "@/app/(editor)/studio/create-blog/[id]/_components/Blocks/SelectBlock";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { useParams } from "next/navigation";
+import { addStyle } from "@/redux/features/builders/blogBuilderSlice";
+
+const objectFitList = [
+  {
+    id: "fill",
+    label: "Fill (Default)",
+  },
+  {
+    id: "contain",
+    label: "Contain",
+  },
+  {
+    id: "cover",
+    label: "Cover",
+  },
+];
+
+const ImageObjectFit = () => {
+  const dispatch = useAppDispatch();
+  const { id: blogId } = useParams<{ id: string }>();
+
+  if (!blogId) return null;
+
+  const {
+    activeBlock,
+    metaData: { styles },
+  } = useAppSelector((state) => state.blogBuilder.blogs[blogId]);
+
+  if (!activeBlock) return null;
+
+  const activeStyle = styles[activeBlock] as CSSProperties;
+
+  const handleChange = (value: string) => {
+    dispatch(
+      addStyle({
+        blogId,
+        activeBlockId: activeBlock,
+        styles: {
+          objectFit: value,
+        },
+      })
+    );
+  };
+
+  return (
+    <SelectBlock
+      label="Object Fit"
+      activeValue={String(activeStyle?.objectFit || objectFitList[0].id)}
+      itemList={objectFitList}
+      handleChange={handleChange}
+    />
+  );
+};
+
+export default ImageObjectFit;
