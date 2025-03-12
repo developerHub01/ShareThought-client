6d057da29846bbc0c5c2c8321cdb61ed4c482fef
developerHub01 <developerhubytchannel@gmail.com>
2025-03-13 03:24:03 +0600

    add: fontSize property reset to default button added

--- src/app/(editor)/studio/create-blog/[id]/_components/PropertiesTab/Common/Font/FontSizeProperty.tsx
+++ src/app/(editor)/studio/create-blog/[id]/_components/PropertiesTab/Common/Font/FontSizeProperty.tsx
@@ -1,16 +1,19 @@
 "use client";
 
-import React, { ChangeEvent, useCallback } from "react";
+import React, { ChangeEvent, useCallback, useMemo } from "react";
 import CountBlock from "@/app/(editor)/studio/create-blog/[id]/_components/Blocks/CountBlock";
 import { addStyle } from "@/redux/features/builders/blogBuilderSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { useParams } from "next/navigation";
 import { EDITOR_DEFAULT_VALUES } from "@/constant";
 import useActiveStylePropertyTab from "@/hooks/editor/use-active-style-property-tab";
+import CheckGlobalStyle from "@/app/(editor)/studio/create-blog/[id]/_components/Blocks/CheckGlobalStyle";
+import useRemoveStyle from "@/hooks/editor/use-remove-style";
 
 const FontSizeProperty = () => {
   const dispatch = useAppDispatch();
   const { id: blogId } = useParams<{ id: string }>();
+  const handleReset = useRemoveStyle();
 
   if (!blogId) return null;
 
@@ -24,6 +27,11 @@ const FontSizeProperty = () => {
   if (!activeBlock) return null;
 
   const { type } = components[activeBlock];
+
+  const haveCustomStyle = useMemo(
+    () => "fontSize" in styles[activeBlock],
+    [activeBlock, styles]
+  );
 
   const activeStyle = useActiveStylePropertyTab({
     type,
@@ -80,6 +88,12 @@ const FontSizeProperty = () => {
       handleChange={handleChange}
       min={EDITOR_DEFAULT_VALUES.FONT_SIZE.MIN}
       max={EDITOR_DEFAULT_VALUES.FONT_SIZE.MAX}
+      AfterComponent={() => (
+        <CheckGlobalStyle
+          disabled={!haveCustomStyle}
+          handleReset={() => handleReset("fontSize")}
+        />
+      )}
     />
   );
 };
