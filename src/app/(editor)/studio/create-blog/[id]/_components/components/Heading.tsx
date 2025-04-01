f285c56f6353bb5ebfb2779ea1003e62385c2f3e
developerHub01 <developerhubytchannel@gmail.com>
2025-04-01 20:02:05 +0600

    refactor: enhance Heading component with memoization and combined responsive styles

--- src/app/(editor)/studio/create-blog/[id]/_components/components/Heading.tsx
+++ src/app/(editor)/studio/create-blog/[id]/_components/components/Heading.tsx
@@ -3,24 +3,44 @@
 import { updateComponentText } from "@/redux/features/builders/blogBuilderSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { useParams } from "next/navigation";
-import React, { FocusEvent } from "react";
+import React, {
+  FocusEvent,
+  memo,
+  useCallback,
+  useEffect,
+  useMemo,
+} from "react";
 import { TYPOGRAPHY_LIST } from "@/constant";
 import {
+  selectBlogActiveBlock,
   selectBlogComponentById,
+  selectBlogGlobalStyle,
+  selectBlogMobileStylesById,
+  selectBlogScreenType,
   selectBlogStylesById,
 } from "@/redux/features/builders/selectors";
+import useCombinedResponsiveSettingStyles from "@/hooks/editor/use-combined-responsive-setting-styles";
 
 interface HeadingProps {
   id: string;
   parentId?: string;
 }
 
-const Heading = ({ id, parentId, ...props }: HeadingProps) => {
+const Heading = memo(({ id, parentId, ...props }: HeadingProps) => {
   const { id: blogId } = useParams<{ id: string }>();
   const dispatch = useAppDispatch();
 
+  const screenType = useAppSelector((state) =>
+    selectBlogScreenType(state, blogId)
+  );
+  const globalStyles = useAppSelector((state) =>
+    selectBlogGlobalStyle(state, blogId)
+  );
   const styles = useAppSelector((state) =>
     selectBlogStylesById(state, blogId, id)
+  );
+  const mobileStyles = useAppSelector((state) =>
+    selectBlogMobileStylesById(state, blogId, id)
   );
   const component = useAppSelector((state) =>
     selectBlogComponentById(state, blogId, id)
@@ -29,6 +49,14 @@ const Heading = ({ id, parentId, ...props }: HeadingProps) => {
   if (!blogId || !component) return null;
 
   const { type, text } = component;
+
+  const combinedStyles = useCombinedResponsiveSettingStyles({
+    type,
+    screenType,
+    styles,
+    mobileStyles,
+    globalStyles,
+  });
 
   const handleBlur = (
     e: FocusEvent<HTMLHeadElement | HTMLParagraphElement>
@@ -46,23 +74,20 @@ const Heading = ({ id, parentId, ...props }: HeadingProps) => {
     TYPOGRAPHY_LIST[type] || TYPOGRAPHY_LIST.h1;
 
   return (
-    <>
-      <style></style>
-      <Tag
-        contentEditable
-        suppressContentEditableWarning
-        data-component-type={type}
-        data-component-id={id}
-        onBlur={handleBlur}
-        className={defaultClassName}
-        style={{
-          ...styles,
-        }}
-      >
-        {text}
-      </Tag>
-    </>
+    <Tag
+      contentEditable
+      suppressContentEditableWarning
+      data-component-type={type}
+      data-component-id={id}
+      onBlur={handleBlur}
+      className={defaultClassName}
+      style={{
+        ...combinedStyles,
+      }}
+    >
+      {text}
+    </Tag>
   );
-};
+});
 
 export default Heading;
