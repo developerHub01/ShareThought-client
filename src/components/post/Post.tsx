79cbef0cce54035a86c977940a26cea7c6e98acc
developerHub01 <developerhubytchannel@gmail.com>
2025-04-02 03:27:55 +0600

    refactor: add banner image support in Post component for enhanced visual presentation

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,14 +1,26 @@
 import { BlogStateInterface } from "@/redux/features/builders/blogBuilderSlice";
 import React from "react";
 import BlockDecision from "@/components/post/components/BlockDecision";
+import Image from "next/image";
 
 const Post = ({ title, content, components, metaData }: BlogStateInterface) => {
+  const bannerImage = metaData?.imgLinks?.banner || "";
+
   return (
     <div className="w-full">
       <div className="w-full max-w-3xl mx-auto">
-        <h1 className="text-4xl font-bold text-primary">
+        <h1 className="text-4xl font-bold text-primary pb-4">
           {title || "Dummy Blog Title"}
         </h1>
+        {bannerImage && (
+          <Image
+            width={800}
+            height={350}
+            alt="banner title"
+            src={bannerImage}
+            className="w-full h-full object-cover mb-4"
+          />
+        )}
 
         <section className="w-full h-full py-5 flex flex-col">
           {content.map((id) => (
