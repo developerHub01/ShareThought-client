a817e513e8f5757b43633df504cdcf571ad76580
developerHub01 <developerhubytchannel@gmail.com>
2025-04-03 04:20:44 +0600

    refactor: working with editor preview. Heading component complete

--- src/app/(editor)/studio/create-blog/[id]/_components/Preview/Components/Heading.tsx
+++ src/app/(editor)/studio/create-blog/[id]/_components/Preview/Components/Heading.tsx
@@ -1,86 +1,64 @@
 "use client";
 
-import { updateComponentText } from "@/redux/features/builders/blogBuilderSlice";
-import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import { useParams } from "next/navigation";
-import React, { FocusEvent, memo } from "react";
-import { TYPOGRAPHY_LIST } from "@/constant";
 import {
-  selectBlogComponentById,
-  selectBlogGlobalStyle,
-  selectBlogMobileStylesById,
-  selectBlogScreenType,
-  selectBlogStylesById,
-} from "@/redux/features/builders/selectors";
+  BlogComponentsDataInterface,
+  BlogContentType,
+  BlogMetaDataInterface,
+} from "@/redux/features/builders/blogBuilderSlice";
+import { useParams } from "next/navigation";
+import React, { memo } from "react";
+import { TYPOGRAPHY_LIST } from "@/constant";
 import useCombinedResponsiveSettingStyles from "@/hooks/editor/use-combined-responsive-setting-styles";
+import { useEditorPreview } from "@/app/(editor)/studio/create-blog/[id]/_context/Preview/EditorPreviewProvider";
 
 interface HeadingProps {
   id: string;
   parentId?: string;
+  content: BlogContentType;
+  components: BlogComponentsDataInterface;
+  metaData: BlogMetaDataInterface;
 }
 
-const Heading = memo(({ id, parentId, ...props }: HeadingProps) => {
-  const { id: blogId } = useParams<{ id: string }>();
-  const dispatch = useAppDispatch();
+const Heading = memo(
+  ({ id, parentId, components, metaData, ...props }: HeadingProps) => {
+    const { id: blogId } = useParams<{ id: string }>();
+    const { screenType } = useEditorPreview();
 
-  const screenType = useAppSelector((state) =>
-    selectBlogScreenType(state, blogId)
-  );
-  const globalStyles = useAppSelector((state) =>
-    selectBlogGlobalStyle(state, blogId)
-  );
-  const styles = useAppSelector((state) =>
-    selectBlogStylesById(state, blogId, id)
-  );
-  const mobileStyles = useAppSelector((state) =>
-    selectBlogMobileStylesById(state, blogId, id)
-  );
-  const component = useAppSelector((state) =>
-    selectBlogComponentById(state, blogId, id)
-  );
+    const globalStyles = metaData.globalStyles;
+    const styles = metaData.styles[id];
+    const mobileStyles = metaData.mobileStyles[id];
+    const component = components[id];
 
-  if (!blogId || !component) return null;
+    if (!blogId || !component) return null;
 
-  const { type, text } = component;
+    const { type, text } = component;
 
-  const combinedStyles = useCombinedResponsiveSettingStyles({
-    type,
-    screenType,
-    styles,
-    mobileStyles,
-    globalStyles,
-  });
+    const combinedStyles = useCombinedResponsiveSettingStyles({
+      type,
+      screenType,
+      styles,
+      mobileStyles,
+      globalStyles,
+    });
 
-  const handleBlur = (
-    e: FocusEvent<HTMLHeadElement | HTMLParagraphElement>
-  ) => {
-    dispatch(
-      updateComponentText({
-        blogId,
-        id,
-        text: e.target.innerText ?? "",
-      })
+    const { tag: Tag, className: defaultClassName } =
+      TYPOGRAPHY_LIST[type] || TYPOGRAPHY_LIST.h1;
+
+    return (
+      <Tag
+        contentEditable
+        suppressContentEditableWarning
+        data-component-type={type}
+        data-component-id={id}
+        className={defaultClassName}
+        style={{
+          ...combinedStyles,
+        }}
+      >
+        {text}
+      </Tag>
     );
-  };
-
-  const { tag: Tag, className: defaultClassName } =
-    TYPOGRAPHY_LIST[type] || TYPOGRAPHY_LIST.h1;
-
-  return (
-    <Tag
-      contentEditable
-      suppressContentEditableWarning
-      data-component-type={type}
-      data-component-id={id}
-      onBlur={handleBlur}
-      className={defaultClassName}
-      style={{
-        ...combinedStyles,
-      }}
-    >
-      {text}
-    </Tag>
-  );
-});
+  }
+);
 
 export default Heading;
