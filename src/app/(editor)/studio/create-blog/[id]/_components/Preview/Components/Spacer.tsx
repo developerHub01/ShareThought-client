7169b1e00fe81b120abb179c867f600b62fec5de
developerHub01 <developerhubytchannel@gmail.com>
2025-04-03 04:33:54 +0600

    refactor: working with editor preview. Spacer component complete

--- src/app/(editor)/studio/create-blog/[id]/_components/Preview/Components/Spacer.tsx
+++ src/app/(editor)/studio/create-blog/[id]/_components/Preview/Components/Spacer.tsx
@@ -3,53 +3,59 @@
 import useCombinedResponsiveSettingStyles from "@/hooks/editor/use-combined-responsive-setting-styles";
 import { cn } from "@/lib/utils";
 import {
-  selectBlogMobileStylesById,
-  selectBlogScreenType,
-  selectBlogStylesById,
-} from "@/redux/features/builders/selectors";
-import { useAppSelector } from "@/redux/hooks";
+  BlogComponentsDataInterface,
+  BlogContentType,
+  BlogMetaDataInterface,
+} from "@/redux/features/builders/blogBuilderSlice";
 import { useParams } from "next/navigation";
 import React, { CSSProperties, memo } from "react";
+import { useEditorPreview } from "@/app/(editor)/studio/create-blog/[id]/_context/Preview/EditorPreviewProvider";
 
 interface SpacerProps {
   id: string;
   parentId?: string;
   className?: string;
+  content: BlogContentType;
+  components: BlogComponentsDataInterface;
+  metaData: BlogMetaDataInterface;
   [key: string]: unknown;
 }
 
-const Spacer = memo(({ id, parentId, className, ...props }: SpacerProps) => {
-  const { id: blogId } = useParams<{ id: string }>();
+const Spacer = memo(
+  ({
+    id,
+    parentId,
+    className,
+    components,
+    metaData,
+    ...props
+  }: SpacerProps) => {
+    const { id: blogId } = useParams<{ id: string }>();
 
-  const screenType = useAppSelector((state) =>
-    selectBlogScreenType(state, blogId)
-  );
-  const styles = useAppSelector((state) =>
-    selectBlogStylesById(state, blogId, id)
-  );
-  const mobileStyles = useAppSelector((state) =>
-    selectBlogMobileStylesById(state, blogId, id)
-  );
+    const { screenType } = useEditorPreview();
+    const styles = metaData.styles[id];
+    const mobileStyles = metaData.mobileStyles[id];
 
-  if (!blogId) return null;
+    if (!blogId) return null;
 
-  const combinedStyles = useCombinedResponsiveSettingStyles({
-    type: "spacer",
-    screenType,
-    styles,
-    mobileStyles,
-  });
+    const combinedStyles = useCombinedResponsiveSettingStyles({
+      type: "spacer",
+      screenType,
+      styles,
+      mobileStyles,
+    });
 
-  return (
-    <div
-      className={cn("", className)}
-      style={{
-        ...(combinedStyles as CSSProperties),
-      }}
-      data-component-type={"spacer"}
-      data-component-id={id}
-    ></div>
-  );
-});
+    return (
+      <div
+        className={cn("", className)}
+        style={{
+          ...(combinedStyles as CSSProperties),
+        }}
+        data-component-type={"spacer"}
+        data-component-id={id}
+      ></div>
+    );
+  }
+);
 
 export default Spacer;
