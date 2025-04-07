7303264a6bf720612f58698c243eeb241f5574db
developerHub01 <developerhubytchannel@gmail.com>
2025-04-07 16:15:39 +0600

    feat: Enhance Code component with EditorView support and improve layout for better code display

--- src/app/(editor)/studio/create-blog/[id]/_components/components/Code.tsx
+++ src/app/(editor)/studio/create-blog/[id]/_components/components/Code.tsx
@@ -2,13 +2,18 @@
 
 import { useParams } from "next/navigation";
 import React, { FocusEvent, useEffect, useState } from "react";
-import CodeMirror, { ViewUpdate } from "@uiw/react-codemirror";
+import CodeMirror, { EditorView, ViewUpdate } from "@uiw/react-codemirror";
 import { githubDark } from "@uiw/codemirror-theme-github";
 import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
 import { languages } from "@codemirror/language-data";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { updateComponentText } from "@/redux/features/builders/blogBuilderSlice";
-import { selectBlogComponentText } from "@/redux/features/builders/selectors";
+import {
+  selectBlogComponentById,
+  selectBlogComponentText,
+  selectBlogStylesById,
+} from "@/redux/features/builders/selectors";
+import handleWrapperContentStyleSeparator from "@/utils/editor/handleWrapperContentStyleSeparator";
 
 interface CodeProps {
   id: string;
@@ -28,10 +33,22 @@ const Code = ({ id, parentId, ...props }: CodeProps) => {
     selectBlogComponentText(state, blogId, id)
   );
 
+  const styles = useAppSelector((state) =>
+    selectBlogStylesById(state, blogId, id)
+  );
+  const component = useAppSelector((state) =>
+    selectBlogComponentById(state, blogId, id)
+  );
+
   useEffect(() => {
     if (syncCode === code) return;
     setCode(syncCode);
   }, [syncCode]);
+
+  if (!blogId || !component) return null;
+
+  let { contentStyles, wrapperStyles } =
+    handleWrapperContentStyleSeparator(styles);
 
   const handleChange = React.useCallback(
     (val: string, viewUpdate: ViewUpdate) => {
@@ -54,17 +71,29 @@ const Code = ({ id, parentId, ...props }: CodeProps) => {
   };
 
   return (
-    <CodeMirror
-      value={code}
-      height="auto"
-      theme={githubDark}
-      extensions={[
-        markdown({ base: markdownLanguage, codeLanguages: languages }),
-      ]}
-      onChange={handleChange}
-      onBlur={handleBlur}
-      autoFocus={true}
-    />
+    <div
+      className="grid"
+      style={{
+        ...wrapperStyles,
+      }}
+    >
+      <CodeMirror
+        className="w-full whitespace-pre-wrap"
+        style={{
+          ...contentStyles,
+        }}
+        value={code}
+        height="auto"
+        theme={githubDark}
+        extensions={[
+          markdown({ base: markdownLanguage, codeLanguages: languages }),
+          EditorView.lineWrapping,
+        ]}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        autoFocus={true}
+      />
+    </div>
   );
 };
 
--- src/components/post/components/Code.tsx
+++ src/components/post/components/Code.tsx
@@ -3,7 +3,7 @@ import {
   BlogContentType,
   BlogMetaDataInterface,
 } from "@/redux/features/builders/blogBuilderSlice";
-import CodeMirror from "@uiw/react-codemirror";
+import CodeMirror, { EditorView } from "@uiw/react-codemirror";
 import { githubDark } from "@uiw/codemirror-theme-github";
 import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
 import { languages } from "@codemirror/language-data";
@@ -23,19 +23,18 @@ const Code = ({ id, parentId, components, ...props }: CodeProps) => {
   const code = components?.[id]?.text ?? "";
 
   return (
-    <div className="w-full overflow-hidden rounded-sm flex">
-      <CodeMirror
-        className="w-full"
-        value={code}
-        height="auto"
-        theme={githubDark}
-        extensions={[
-          markdown({ base: markdownLanguage, codeLanguages: languages }),
-        ]}
-        editable={false}
-        readOnly={false}
-      />
-    </div>
+    <CodeMirror
+      value={code}
+      height="auto"
+      width="auto"
+      theme={githubDark}
+      extensions={[
+        markdown({ base: markdownLanguage, codeLanguages: languages }),
+        // EditorView.lineWrapping,
+      ]}
+      editable={false}
+      readOnly={false}
+    />
   );
 };
 
