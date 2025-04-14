503abb20d6afc740c4c68ed5513f4d55f763a0b7
developerHub01 <developerhubytchannel@gmail.com>
2025-04-14 07:44:29 +0600

    feat: add ButtonGroup component for flexible button arrangement with optional separators

---
+++ src/components/ui/button-group.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
+import React, { Fragment, ReactNode } from "react";
+
+interface ButtonGroupProps {
+  children: ReactNode[];
+  showSeparators?: boolean;
+  size?: "sm" | "lg" | "default";
+  variant?: "default" | "outline" | "ghost" | "link";
+  orientation?: "vertical" | "horizontal";
+  className?: string;
+}
+
+export const ButtonGroup = ({
+  children,
+  showSeparators = true,
+  className = "",
+  size = "default",
+  variant = "default",
+  orientation = "horizontal",
+}: ButtonGroupProps) => {
+  return (
+    <div
+      className={cn("inline-flex ml-auto items-center", className, {
+        "flex-row": orientation === "horizontal",
+        "flex-col": orientation === "vertical",
+      })}
+    >
+      {children.map((child, index) => {
+        const isFirst = index === 0;
+        const isLast = index === children.length - 1;
+        return (
+          <Fragment key={index}>
+            {React.cloneElement(child as React.ReactElement<any>, {
+              className: cn(
+                (child as React.ReactElement<any>).props.className || "",
+                {
+                  "rounded-r-none": isFirst && orientation === "horizontal",
+                  "rounded-l-none": isLast && orientation === "horizontal",
+                  "rounded-b-none": isFirst && orientation === "vertical",
+                  "rounded-t-none": isLast && orientation === "vertical",
+                  "rounded-none": !isFirst && !isLast,
+                }
+              ).trim(),
+              size: (child as React.ReactElement<any>).props.size || size,
+              variant:
+                (child as React.ReactElement<any>).props.variant || variant,
+            })}
+            {showSeparators && !isLast && (
+              <Separator
+                orientation={
+                  orientation === "horizontal" ? "vertical" : "horizontal"
+                }
+              />
+            )}
+          </Fragment>
+        );
+      })}
+    </div>
+  );
+};
