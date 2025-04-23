47fcd3410f5817cddc2deaf3b7bfe8775466134a
developerHub01 <developerhubytchannel@gmail.com>
2025-04-23 21:08:00 +0600

    refactor: improve styling and responsiveness of BannerCanvas component

--- src/app/(editor)/studio/create-blog/[id]/_components/Banner/BannerCanvas.tsx
+++ src/app/(editor)/studio/create-blog/[id]/_components/Banner/BannerCanvas.tsx
@@ -5,9 +5,9 @@ import CanvasWrapper from "@/app/(editor)/studio/create-blog/[id]/_components/Ba
 const BannerCanvas = () => {
   return (
     <CanvasWrapper>
-      <div className="flex flex-col justify-center items-center gap-4 z-10">
-        <ImageIcon size={60} />
-        <p>Drop blog banner here...</p>
+      <div className="flex flex-col justify-center items-center gap-4 z-10 text-center">
+        <ImageIcon className="size-6 md:size-12 lg:size-20" />
+        <p className="text-xs md:text-base">Drop blog banner here...</p>
         <label
           htmlFor="bannerImageUploader"
           className="cursor-pointer bg-primary-foreground hover:bg-primary-foreground/80 text-primary px-4 py-1.5 rounded-sm"
